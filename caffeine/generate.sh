0e84e323df049389482335951f9a63191e34f8b1
Stig Rune Jensen <stig.r.jensen@uit.no>
2020-11-13 20:44:34 +0100

    Update caffeine

--- caffeine/generate.sh
+++ caffeine/generate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 
-mol=$1
+mol="caffeine"
+method=$1
 nodes=$2
 tasks_per_node=$3
 cpus_per_task=$4
@@ -13,11 +14,12 @@ root_path="\/cluster\/home\/stig\/src\/mrchem-benchmark"
 mol_path="/cluster/home/stig/src/mrchem-benchmark/molecules/${mol}"
 inp_name="nodes_${nodes}_tasks_${tasks_per_node}_omp_${cpus_per_task}"
 
-mkdir -p ${mol}
-cd ${mol}
+mkdir -p ${method}
+cd ${method}
 
 cp ../template.inp                           ${inp_name}.inp
 sed -i "/coords/r ${mol_path}/${mol}.xyz"    ${inp_name}.inp
+sed -i "s/METHOD/${method}/"                 ${inp_name}.inp
 
 cp ../template.run                           ${inp_name}.run
 sed -i "s/NAME/${mol}/"                      ${inp_name}.run
